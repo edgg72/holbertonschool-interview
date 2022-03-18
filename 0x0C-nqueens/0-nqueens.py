a66ec76b2fe547be411140881a2ad6f453d8c4ef
Ever Daniel Gonzalez <edgg72@hotmail.com>
2022-03-18 07:42:47 -0500

    Add files via upload

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -1,68 +1,43 @@
 #!/usr/bin/python3
-"""Solution to N Queens problem"""
-import sys
+'''
+Module that solves the N Queens puzzle
+'''
+from sys import argv, exit
 
-
-def format(board):
-    """Prints according to requirements"""
-    ret = []
-    for i in range(len(board)):
-        colIdx = board[i].index(1)
-        ret.append([i, colIdx])
-    print(ret)
-
-
-def isValid(board, curCol, row, n):
-    """Checks if board[row][curCol] is a valid queen"""
-    # Check prev columns
-    for i in range(curCol):
-        if board[row][i] == 1:
-            return False
-    # Check for upper diagonal
-    i = row
-    j = curCol
-    while i >= 0 and j >= 0:
-        if board[i][j] == 1:
-            return False
-        i -= 1
-        j -= 1
-    # Check for lower diagonal
-    i = row
-    j = curCol
-    while i < n and j >= 0:
-        if board[i][j] == 1:
-            return False
-        i += 1
-        j -= 1
-    return True
-
-
-def nQueens(board, curCol, n):
-    """Recursive call that places queens in all the
-    posible positions of the board"""
-    stat = False
-    if curCol == n:
-        format(board)
-        return True
-    for row in range(0, n):
-        if isValid(board, curCol, row, n):
-            board[row][curCol] = 1
-            stat = nQueens(board, curCol + 1, n) or stat
-            board[row][curCol] = 0
-    return stat
-
-
-if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
+if __name__ == "__main__":
+    if len(argv) != 2:
+        print('Usage: nqueens N')
         exit(1)
-    if not sys.argv[1].isdigit():
-        print("N must be a number")
+    try:
+        n = int(argv[1])
+    except BaseException:
+        print('N must be a number')
         exit(1)
-    n = int(sys.argv[1])
     if n < 4:
-        print("N must be at least 4")
+        print('N must be at least 4')
         exit(1)
-    board = [[0 for i in range(n)]for j in range(n)]
-    # first col is 0
-    nQueens(board, 0, n)
+
+    solution = []
+
+    def solve_queens(row, n, solution):
+        if (row == n):
+            print(solution)
+        else:
+            for col in range(n):
+                placement = [row, col]
+                if valid_placement(solution, placement):
+                    solution.append(placement)
+                    solve_queens(row + 1, n, solution)
+                    solution.remove(placement)
+
+    def valid_placement(solution, placement):
+        for queen in solution:
+            if queen[1] == placement[1]:
+                return False
+            if (queen[0] + queen[1]) == (placement[0] + placement[1]):
+                return False
+            if (queen[0] - queen[1]) == (placement[0] - placement[1]):
+                return False
+        return True
+
+    solve_queens(0, n, solution)
